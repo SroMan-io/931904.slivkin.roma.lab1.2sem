a58c18a129d5389562d9a6391fb5820db0a593c9
SroMan-io <85176272+SroMan-io@users.noreply.github.com>
2022-03-02 22:49:56 +0700

    Add files via upload

---
+++ Models/CalcServices.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1.Models
+{
+
+    public class CalcServices
+    {
+        public Random rand = new Random();
+
+        public int x, y, add, sub, mult, div;
+        
+        public CalcServices() { x = rand.Next(0, 11); y = rand.Next(0, 11); add = x + y; sub = x - y; mult = x * y;
+            if (y == 0)  div = 100;
+            else div = x / y; }        
+    }
+}
