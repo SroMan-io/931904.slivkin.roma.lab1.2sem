0e8edc95a256d59228a8f42ffa5a2946a43aca70
SroMan-io <85176272+SroMan-io@users.noreply.github.com>
2022-03-02 22:44:17 +0700

    Add files via upload

---
+++ Services/CalcService.cs
@@ -0,0 +1,26 @@
+﻿namespace WebApplication1.Services
+{
+
+    public static class ServiceProviderExtensions
+    {
+        public static void AddCalcService(this IServiceCollection services)
+        {
+            services.AddTransient<CalcService>();
+        }
+    }
+    public class CalcService
+    {
+        
+            public Random rand = new Random();
+
+            public int x, y, add, sub, mult, div;
+
+            public CalcService()
+            {
+                x = rand.Next(0, 11); y = rand.Next(0, 11); add = x + y; sub = x - y; mult = x * y;
+                if (y == 0) div = 100;
+                else div = x / y;
+            }
+        }
+    
+}
